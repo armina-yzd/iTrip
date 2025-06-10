d90c71d7c7257d2ea4953c4b6a65be38782dad7f
narjesgorjii <narjesgorjii83@gmail.com>
2025-06-11 01:07:58 +0330

    dockerize front

--- deployment/deploy.sh
+++ deployment/deploy.sh
@@ -1,7 +1,7 @@
 docker compose -f docker-compose.yml \
                -f ../api_gateway/docker-compose.yml \
-               -f ../back/services/IAM/docker-compose.yml \
-               -f ../back/services/manage_services/docker-compose.yml \
-               -f ../back/services/user_ticket/docker-compose.yml \
+               -f ../services/IAM/docker-compose.yml \
+               -f ../services/manage_services/docker-compose.yml \
+               -f ../services/user_ticket/docker-compose.yml \
                -f ../front/docker-compose.yml \
                up -d --build
